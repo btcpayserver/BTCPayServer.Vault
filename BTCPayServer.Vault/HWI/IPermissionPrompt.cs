5701e898899e4c3d04992f809cdc12e441a93d42
nicolas.dorier <nicolas.dorier@gmail.com>
2019-11-19 22:03:23 +0900

    missing file

---
+++ BTCPayServer.Vault/HWI/IPermissionPrompt.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BTCPayServer.Vault.HWI
+{
+    public interface IPermissionPrompt
+    {
+        Task<bool> AskPermission(string origin, CancellationToken cancellationToken);
+    }
+}
