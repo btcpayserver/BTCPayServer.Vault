abf1afa4608613de3c0604a1e22ee813ea506158
nicolas.dorier <nicolas.dorier@gmail.com>
2019-11-26 11:40:39 +0900

    Fix warning

--- BTCPayServer.Vault/MainWindowViewModel.cs
+++ BTCPayServer.Vault/MainWindowViewModel.cs
@@ -17,7 +17,9 @@ namespace BTCPayServer.Vault
             {
                 this.act = act;
             }
+#pragma warning disable CS0067
             public event EventHandler CanExecuteChanged;
+#pragma warning restore CS0067
 
             public bool CanExecute(object parameter)
             {
