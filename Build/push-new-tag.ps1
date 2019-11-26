f9b25641088328b8605d0e189b19bd0aa71d9cca
nicolas.dorier <nicolas.dorier@gmail.com>
2019-11-26 18:10:21 +0900

    Fix push new tag

--- Build/push-new-tag.ps1
+++ Build/push-new-tag.ps1
@@ -1,3 +1,3 @@
-$ver = [regex]::Match((Get-Content ../BTCPayServer.Vault/BTCPayServer.Vault.csproj), '<Version>([^<]+)<').Groups[1].Value
+$ver = [regex]::Match((Get-Content ../BTCPayServer.Vault/Version.csproj), '<Version>([^<]+)<').Groups[1].Value
 git tag -a "Vault/v$ver" -m "Vault/v$ver"
 git push origin "Vault/v$ver"
