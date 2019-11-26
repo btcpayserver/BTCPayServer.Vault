d50124e1f8a5d5d2f66bb449f9f1f0f94d70dace
nicolas.dorier <nicolas.dorier@gmail.com>
2019-11-26 12:07:59 +0900

    Explain signature

--- Build/build.ps1
+++ Build/build.ps1
@@ -9,5 +9,5 @@ foreach ($arch in "debian-x64","linux-x64","win-x64","osx-x64")
 }
 docker build -t "vault-hashes" -f "hashes/Dockerfile" "hashes"
 docker run --rm -v "${distFolder}:/opt/dist" "vault-hashes"
-keybase pgp sign -i "${distFolder}/SHA256SUMS" -k "ab4cfa9895aca0dbe27f6b346618763ef09186fe" -c -t -o "${distFolder}/SHA256SUMS.asc"
+keybase pgp sign -i "${distFolder}/SHA256SUMS" -k "015b4c837b245509e4ac8995223fda69debea82d" -c -t -o "${distFolder}/SHA256SUMS.asc"
 Remove-Item "${distFolder}/SHA256SUMS"
