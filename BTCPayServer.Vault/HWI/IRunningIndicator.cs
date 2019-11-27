4ce769889df4c78bccddd76a696d2e4d2f220a9b
nicolas.dorier <nicolas.dorier@gmail.com>
2019-11-27 17:41:16 +0900

    Missing files

---
+++ BTCPayServer.Vault/HWI/IRunningIndicator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BTCPayServer.Vault.HWI
+{
+    public interface IRunningIndicator
+    {
+        public event Action<object, string> Running;
+        public event EventHandler StoppedRunning;
+    }
+}
---
+++ BTCPayServer.Vault/HWI/InternalTransport.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using BTCPayServer.Hwi.Transports;
+using System.Linq;
+
+namespace BTCPayServer.Vault.HWI
+{
+    internal class InternalTransport : ITransport, IRunningIndicator
+    {
+        private readonly ITransport _inner;
+        public InternalTransport(ITransport inner)
+        {
+            _inner = inner;
+        }
+        public event Action<object, string> Running;
+        public event EventHandler StoppedRunning;
+        public async Task<string> SendCommandAsync(string[] arguments, CancellationToken cancel)
+        {
+            try
+            {
+                Running?.Invoke(this, arguments.FirstOrDefault());
+                return await _inner.SendCommandAsync(arguments, cancel);
+            }
+            finally
+            {
+                StoppedRunning?.Invoke(this, new EventArgs());
+            }
+        }
+    }
+}
