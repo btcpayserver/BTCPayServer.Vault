f19ad70b7e5c71088f19912c89e746c13ae7ac3e
nicolas.dorier <nicolas.dorier@gmail.com>
2019-12-05 19:00:03 +0900

    Check Travis tag is coherant with csproj

--- Build/travis/build.sh
+++ Build/travis/build.sh
@@ -6,6 +6,16 @@ DOCKER_IMAGE_NAME="vault-$RID"
 docker build -t "$DOCKER_IMAGE_NAME" $DOCKER_BUILD_ARGS -f "Build/$RID/Dockerfile" .
 docker run --rm -v "$(pwd)/dist:/opt/dist" "$DOCKER_IMAGE_NAME"
 
+if [[ "$TRAVIS_TAG" ]]; then
+    travis_version="$(echo "$TRAVIS_TAG" | cut -d'/' -f2)"
+    csproj_version="v$(cat BTCPayServer.Vault/Version.csproj | sed -n 's/.*<Version>\(.*\)<\/Version>.*/\1/p')"
+    if [[ "$travis_version" != "$csproj_version" ]]; then
+        echo "The tagged version on travis ($travis_version) is different from the csproj ($csproj_version)"
+        exit 1
+    fi
+    exit 0
+fi
+
 if ! [[ "$AZURE_STORAGE_CONNECTION_STRING" ]] || ! [[ "$AZURE_STORAGE_CONTAINER" ]]; then
     echo "Skipped upload to Azure (AZURE_STORAGE_CONNECTION_STRING or AZURE_STORAGE_CONTAINER not set)"
     exit 0
