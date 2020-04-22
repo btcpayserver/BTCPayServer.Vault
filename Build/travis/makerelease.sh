cb1d0b50a48f272599affc5c45a34c0e33fde251
nicolas.dorier <nicolas.dorier@gmail.com>
2020-04-22 19:19:02 +0900

    Fix script for makerelease

--- Build/travis/makerelease.sh
+++ Build/travis/makerelease.sh
@@ -21,7 +21,10 @@ DIRECTORY_NAME="dist-$TRAVIS_BUILD_ID"
 wget -O azcopy.tar.gz https://aka.ms/downloadazcopy-v10-linux
 tar -xf azcopy.tar.gz --strip-components=1
 mkdir -p dist
-./azcopy cp "https://$AZURE_ACCOUNT_NAME.blob.core.windows.net/$AZURE_STORAGE_CONTAINER/$DIRECTORY_NAME/*" "dist"
+# Our container is public, so the SAS token should not be needed
+# But AzCopy is broken https://github.com/Azure/azure-storage-azcopy/issues/971
+./azcopy cp "https://$AZURE_ACCOUNT_NAME.blob.core.windows.net/$AZURE_STORAGE_CONTAINER/$DIRECTORY_NAME/*?sv=2019-02-02&ss=b&srt=co&sp=rl&se=2100-04-21T15:00:00Z&st=2020-04-21T19:07:13Z&spr=https&sig=5hMGP4ZR3MUVVp4AVxFDS%2BuFY%2FsU4M8%2B2wKOr8utpWI%3D" \
+            "dist"
 
 release="$(cat Build/RELEASE.md)"
 version="$(echo "$TRAVIS_TAG" | cut -d'/' -f2)"
